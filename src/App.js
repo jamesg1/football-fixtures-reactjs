6eb723e777bf7a9d541258200bf1a77fed6bc7e2
James G <jamesgosbell@gmail.com>
2017-10-17 01:24:09 +1000

    Added fetch url for fixtures list

--- src/App.js
+++ src/App.js
@@ -3,7 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {data: ''}
+  }
+
+  componentWillMount() {
+    const url = 'https://api.ffa.football/t1962/fixture';
+    fetch(url)
+    .then(response => response.json())
+    .then(data => this.setState({ data: data }))
+    .catch(err => console.error(url, err.toString()))
+  }
+
   render() {
+    const data = JSON.stringify(this.state.data);
     return (
       <div className="App">
         <header className="App-header">
@@ -13,6 +28,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {data}
       </div>
     );
   }
